7d7b44b1bb804f7841d9d3efd842222a53f9e5bb
Nathan Villicana-Shaw <nathan@Nathans-MacBook-Pro.local>
2021-03-28 12:28:02 -0700

    Fixed many issues with the header and footer sections, now working on getting the project name to link to the project page within the archive portion of the website

---
+++ php/contact_form_actions.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("nathanshawsemail@gmail.com", "Website Contact",
+
+
+$_POST["TODO - need to get the data from the form into here"]. "From: nathanshawsemail@gmail.com");
+
+
+}
+
+
+?>
